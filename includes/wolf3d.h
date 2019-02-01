bd9f5774ae625a146f357f0e0a4e0cda204a61cc
jchardin <jerome.chardin@outook.com>
2019-02-01 14:23:22 +0100

    ok

---
+++ includes/wolf3d.h
@@ -0,0 +1,16 @@
+#ifndef WOLF3D_H
+# define WOLF3D_H
+
+# define FAILURE 0
+# define SUCESS 1
+
+# include "SDL2/SDL.h"
+# include "SDL2/SDL_image.h"
+# include <stdio.h>
+# include "../libraries/libft/libft.h"
+# include <errno.h>
+
+
+void	ft_quit_error(char *my_message);
+
+#endif
--- wolf3d.h
+++
@@ -1,9 +0,0 @@
-#ifndef WOLF3D_H
-# define WOLF3D_H
-
-# include "SDL2/SDL.h"
-# include "SDL2/SDL_image.h"
-
-
-
-#endif
