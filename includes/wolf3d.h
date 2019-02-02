0b255f88c9d72951c341718a5eb416f5888f2f7c
jchardin <jerome.chardin@outook.com>
2019-02-02 15:30:16 +0100

    double buf strange

--- includes/wolf3d.h
+++ includes/wolf3d.h
@@ -10,16 +10,51 @@
 # include "../libraries/libft/libft.h"
 # include <errno.h>
 
+
+typedef struct			s_my_point
+{
+	int					x;
+	int					y;
+}						t_my_point;
+
+typedef struct			s_my_size
+{
+	int					width;
+	int					height;
+}						t_my_size;
+
+typedef struct			s_my_rectangle
+{
+	t_my_point			point;
+	t_my_size			size;
+}						t_my_rectangle;
+
+
 typedef struct			s_my_win
 {
 	int					i;
 	SDL_Window			*window;
 	SDL_Renderer		*renderer;
 	SDL_Event			event;
+	t_my_size			win_size;
 }						t_my_win;
+
+typedef struct			s_my_input
+{
+	SDL_bool			key[SDL_NUM_SCANCODES];
+	SDL_bool			quit;
+	int					x;
+	int					y;
+	int					xrel;
+	int					yrel;
+	int					xwheel;
+	int					ywheel;
+	SDL_bool			mouse[6];
+}						t_my_input;
 
 
 void					ft_quit(t_my_win *s_win, int status);
 void					ft_show_error_and_quit(t_my_win *s_win, const char *my_message);
+void					ft_get_mouse_position(void);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,40 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 14:22:40 by jchardin          #+#    #+#             */
-/*   Updated: 2019/02/01 19:02:07 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/02/02 15:09:49 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
+void			ft_clear_window(t_my_win *s_win)
+{
+	int		x;
+	int		y;
+
+	SDL_SetRenderDrawColor(s_win->renderer, 0, 0, 0, 0);
+	y = 0;
+	while (y < s_win->win_size.height)
+	{
+		x = 0;
+		while (x < s_win->win_size.width)
+		{
+			SDL_RenderDrawPoint(s_win->renderer, x, y);
+			x++;
+		}
+		y++;
+	}
+	SDL_RenderPresent(s_win->renderer);
+}
+
 
 void			ft_create_window(t_my_win *s_win)
 {
+	s_win->win_size.width = 640;
+	s_win->win_size.height = 480;
 	if (0 != SDL_Init(SDL_INIT_EVERYTHING))
 		ft_show_error_and_quit(s_win, SDL_GetError());
-	s_win->window = SDL_CreateWindow("Wolf", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+	s_win->window = SDL_CreateWindow("Wolf", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, s_win->win_size.width, s_win->win_size.height, SDL_WINDOW_SHOWN);
 	if (s_win->window == NULL)
 		ft_show_error_and_quit(s_win, SDL_GetError());
 	SDL_RaiseWindow(s_win->window); //met la fenetre au premier plan
@@ -30,21 +52,74 @@ void			ft_create_renderer(t_my_win *s_win)
 		ft_show_error_and_quit(s_win, SDL_GetError());
 }
 
-void			ft_event_loop(t_my_win *s_win)
+void			ft_get_mouse_position(void)
 {
-	while(1)
+	int		x;
+	int		y;
+
+	SDL_PumpEvents();
+	SDL_GetMouseState(&x, &y);
+	if (x != 0 && y != 0)
+	printf("mmouse x=%d, y=%d\n", x, y);
+}
+
+void			ft_update_event(t_my_input *s_input)
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
 	{
-		SDL_PollEvent(&(s_win->event));
-		if(s_win->event.type == SDL_QUIT)
-			ft_quit(s_win, SUCESS);
-		if(s_win->event.key.keysym.sym == SDLK_ESCAPE)
-			ft_quit(s_win, SUCESS);
-		if(s_win->event.type == SDL_MOUSEBUTTONDOWN)
+		if (event.type == SDL_QUIT)
+			s_input->quit = SDL_TRUE;
+		else if (event.type == SDL_KEYDOWN)
+			s_input->key[event.key.keysym.scancode] = SDL_TRUE;
+		else if (event.type == SDL_KEYUP)
+			s_input->key[event.key.keysym.scancode] = SDL_FALSE;
+	}
+}
+
+void			ft_draw_rectangle(t_my_rectangle s_rectangle, t_my_win *s_win)
+{
+	int			x;
+	int			y;
+
+	SDL_SetRenderDrawColor(s_win->renderer, 255, 255, 255, 0);
+	y = s_rectangle.point.y;
+	while (y < s_rectangle.point.y + s_rectangle.size.height)
+	{
+		x = s_rectangle.point.x;
+		while(x < s_rectangle.point.x + s_rectangle.size.height)
 		{
-			if (s_win->event.button.button == SDL_BUTTON_LEFT)
-				printf("clic\n");
+			SDL_RenderDrawPoint(s_win->renderer, x, y);
+			x++;
 		}
-		printf("bla\n");
+		y++;
+	}
+	SDL_RenderPresent(s_win->renderer);
+}
+
+void			ft_event_loop(t_my_input *s_input, t_my_win *s_win)
+{
+	t_my_rectangle		s_rectangle;
+	s_rectangle.point.x = 11;
+	s_rectangle.point.y = 11;
+	s_rectangle.size.height = 50;
+	s_rectangle.size.width = 50;
+	while(!s_input->quit)
+	{
+		ft_update_event(s_input);
+		if(s_input->key[SDL_SCANCODE_A])
+		{
+			printf("appui sur A\n");
+			ft_draw_rectangle(s_rectangle, s_win);
+			s_input->key[SDL_SCANCODE_A] = SDL_FALSE;
+		}
+		if(s_input->key[SDL_SCANCODE_C])
+		{
+			printf("appui sur C\n");
+			ft_clear_window(s_win);
+			s_input->key[SDL_SCANCODE_C] = SDL_FALSE;
+		}
+		SDL_Delay(20);
 	}
 }
 
@@ -52,22 +127,19 @@ void			ft_map_editor(t_my_win *s_win)
 {
 	ft_create_window(s_win);
 	ft_create_renderer(s_win);
-	SDL_SetRenderDrawColor(s_win->renderer, 255, 255, 255, 255);
-
-
-	SDL_RenderDrawPoint(s_win->renderer, 10, 10);
+	SDL_SetRenderDrawColor(s_win->renderer, 255, 255, 255, 0);
+	printf("hello\n");
+	SDL_RenderDrawPoint(s_win->renderer, 5, 5);
 	SDL_RenderPresent(s_win->renderer);
+	ft_clear_window(s_win);
 
 
+	t_my_input		s_input;
+	ft_event_loop(&s_input, s_win);
 
 
-
-	ft_event_loop(s_win);
-
-
+	//ft_event_loop(s_win);
 }
-
-
 
 int				main(int argc, char **argv)
 {
@@ -76,7 +148,5 @@ int				main(int argc, char **argv)
 	t_my_win	s_win;
 
 	ft_map_editor(&s_win);
-
 	return (SUCESS);
 }
-
